ced2d512769ee1d8e99567d6115145eea1b22489
abhijeetGupta7 <127014104+abhijeetGupta7@users.noreply.github.com>
2024-05-31 09:31:22 +0530

    Basic Typescript Express setup

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";                      // we can use cjs import also
+import serverConfig from "./config/server.config";
+
+const app=express();
+
+app.listen(serverConfig.PORT,()=>{
+    console.log("Server is listening at port",serverConfig.PORT);
+    console.log("Badi garmi h");
+    console.log("Apne pr rakh visvash abnde")
+    console.log("Ms Dhoni");
+})
