21f58b29d50d76a4b47e0de8469ef135c17b0bfe
abhijeetGupta7 <127014104+abhijeetGupta7@users.noreply.github.com>
2024-05-31 16:35:10 +0530

    Added eslint configurations

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from "express";                      // we can use cjs import also
-import serverConfig from "./config/server.config";
+import express from 'express'; // we can use cjs import also
 
-const app=express();
+import serverConfig from './config/server.config';
 
-app.listen(serverConfig.PORT,()=>{
-    console.log("Server is listening at port",serverConfig.PORT);
-    console.log("Badi garmi h");
-    console.log("Apne pr rakh visvash abnde")
-    console.log("Ms Dhoni");
-})
+const app = express();
+app.listen(serverConfig.PORT, () => {
+  console.log('Server is listening at port', serverConfig.PORT);
+  console.log('Badi garmi h');
+  console.log('Apne pr rakh visvash abnde');
+  console.log('Ms Dhoni');
+});
