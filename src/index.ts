82df6260178b7b4f4b5ba5d440d872ade0a28ab8
abhijeetGupta7 <127014104+abhijeetGupta7@users.noreply.github.com>
2024-06-03 00:22:36 +0530

    Some small updations in Queue setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import express, {Express} from "express"; // we can use cjs import also
 
 import serverConfig from "./config/server.config";
@@ -12,9 +11,6 @@ app.use("/api",apirouter);
 
 app.listen(serverConfig.PORT, () => {
   console.log("Server is listening at port", serverConfig.PORT);
-  console.log("Badi garmi h");
-  console.log("Apne pr rakh visvash abnde");
-  console.log("Ms Dhoni777");
 });
 
 sampleQueueProducer("SampleJob",{
